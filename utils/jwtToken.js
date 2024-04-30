b31d2b0c2e0d6888886d7a0de568645540b059ee
Roushan <roushansingh3698@gmail.com>
2024-04-30 23:35:10 +0530

    first commit

---
+++ utils/jwtToken.js
@@ -0,0 +1,24 @@
+// Creating Token and Saving in Cookie
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getJWTToken();
+
+    // console.log(token)
+
+    // options for cookie
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+    };
+    // console.log(options);
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        user,
+        token
+    });
+};
+
+module.exports = sendToken;
