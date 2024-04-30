7a74db35faff321cb5d0a8dbf130afdd6bc0ad7b
Roushan <roushansingh3698@gmail.com>
2024-05-01 02:46:46 +0530

    custom cors headers

--- app.js
+++ app.js
@@ -21,12 +21,19 @@ app.use(cors({
     origin: ["https://ecommerce-client-brown.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
+}));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload({
     useTempFiles: true
 }));
+
+// Custom CORS headers
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 
 
 // Route imports
