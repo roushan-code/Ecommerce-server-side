c3ef34c96acfa037286d05e62ec94c9627f8eb5c
Roushan <roushansingh3698@gmail.com>
2024-05-05 19:47:52 +0530

    cors allow for all

--- app.js
+++ app.js
@@ -16,25 +16,21 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({ path: "config/config.env" });
 }
 
+app.use(cors());
 app.use(express.json());
-app.use(cors({
-    origin: ["https://vite-commerce-five.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload({
     useTempFiles: true
 }));
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//       next();
+//     });
 
 
 
