6c8bb910606a6599ecc1b88bee5a80c61b43e627
Roushan <roushansingh3698@gmail.com>
2024-05-08 14:26:58 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({ path: "config/config.env" });
 }
 
-app.use(cors());
+// let's tackle cors
+const corsOptions ={
+    origin:'*', 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary")
 const connectDatabase = require("./config/database");
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5000;
 
 
 // Handling Uncaught Exception/Error
