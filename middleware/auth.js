5ac51aa7872b99a99f0cdbafa8db1d3f5f1ff5f5
Roushan <roushansingh3698@gmail.com>
2024-05-12 00:39:15 +0530

    main package.json

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     const { token } = req.cookies;
-
+    
     if (!token) {
         return next(new ErrorHandler("Please Login to access this resource", 401));
     }
