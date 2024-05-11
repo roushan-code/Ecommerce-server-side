920f1cb0a96bbbd783c03b29a68ac22ade190bca
Roushan <roushansingh3698@gmail.com>
2024-05-11 23:29:09 +0530

    try front url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
 
 app.use(cors(
     {
-        origin: 'https://vite-commerce-ecfj.vercel.app/',
+        origin: 'https://vite-commerce-eta.vercel.app',
         credentials: true,
     }
 ));
