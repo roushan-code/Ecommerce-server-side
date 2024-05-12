5e624dc154e1181f5538dd52f746fba7fa1902c2
Roushan <roushansingh3698@gmail.com>
2024-05-12 10:32:19 +0530

    cors functions

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(cors(
         credentials: true,
     }
 ));
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -31,13 +38,6 @@ app.use(fileUpload({
     useTempFiles: true
 }));
 
-// app.use(function (req, res, next) {
-//     //Enabling CORS
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//       next();
-//     });
 
 
 
