e99eecf1ff959b8d1c14d5e2b15c731300bd8600
Roushan <roushansingh3698@gmail.com>
2024-05-12 21:12:00 +0530

    production and development comment

--- app.js
+++ app.js
@@ -17,20 +17,23 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
 }
 
 
+// for development
+// app.use(cors(
+//     {
+//         origin: 'http://localhost:5173',
+//         credentials: true,
+//     }
+// ));
 
+
+// for production
 app.use(cors(
     {
         origin: 'https://vite-commerce.onrender.com',
         credentials: true,
     }
 ));
-// app.use(function (req, res, next) {
-//     //Enabling CORS
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//       next();
-//     });
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
